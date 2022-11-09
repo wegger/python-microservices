3136911d386ea789a4f258574e10901c8318c86f
Wanda Egger <6162870+wegger@users.noreply.github.com>
2022-11-09 19:34:34 +0100

    Add files via upload

---
+++ cast_service/app/api/models.py
@@ -0,0 +1,12 @@
+from typing import List, Optional
+from pydantic import BaseModel
+
+class CastIn(BaseModel):
+    name: str
+    nationality: Optional[str] = None
+
+class CastOut(CastIn):
+    id: int
+
+class CastUpdate(CastIn):
+    name: Optional[str] = None
